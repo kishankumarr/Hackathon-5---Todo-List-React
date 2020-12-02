1e9c9e8c85ed98372c6c53cae2ce6f641be99973
kishan kumar a <kishankumar12324@gmail.com>
2020-12-03 00:38:41 +0530

    commited

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,36 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ListItem from "./ListItem"
 import "./../styles/App.css";
 
 function App() {
-	const [items,setItems]=useState([])
-	const [newitem,setnewItem]=useState("")
+	const [items, setItems] = useState([])
+	const [newitem, setnewItem] = useState("")
 	const additem = () => {
 		items.push(newitem);
 		setItems(items);
 		setnewItem("");
 	}
-	const newitemchanged = (evt)=>{
+	const newitemchanged = (evt) => {
 		setnewItem(evt.target.value);
 	};
-	const deleteHandler=(itemIdx)=>{
-		items.splice(itemIdx,1);
+	const deleteHandler = (itemIdx) => {
+		items.splice(itemIdx, 1);
 		setItems([...items]);
 	}
-	const edithandler=(editedvalue,itemIdx)=>{
-		items[itemIdx]=editedvalue;
+	const edithandler = (editedvalue, itemIdx) => {
+		items[itemIdx] = editedvalue;
 		setItems([...items]);
 	}
 	return (
-	<div id="main">
-    <textarea id="task" onChange={newitemchanged} placeholder="add item" value={newitem}></textarea>
-	<button id="btn" onClick={additem}  disabled={newitem.trim().length===0}>Add</button>
-	{
-		items.map((item,idx)=>(
-			<ListItem item={item} idx={idx} key={`${item}_${idx}`} edithandler={edithandler} deleteHandler={deleteHandler}/>
-		))
-	}
-	</div>
+		<div id="main">
+			<textarea id="task" onChange={newitemchanged} placeholder="add item" value={newitem}></textarea>
+			<button id="btn" onClick={additem} disabled={newitem.trim().length === 0}>Add</button>
+			{
+				items.map((item, idx) => (
+					<ListItem item={item} key={`${item}_${idx}`} idx={idx} edithandler={edithandler} deleteHandler={deleteHandler} />
+				))
+			}
+		</div>
 	);
 }
 
